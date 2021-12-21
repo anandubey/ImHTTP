81f228d7c10958efc7178cf994249bca7a095616
Anand Kumar Dubey <dubey.anandkr@gmail.com>
2021-12-21 12:17:06 +0530

    req exchanging through socket

--- imhttp.h
+++ imhttp.h
@@ -1,25 +1,35 @@
 #ifndef IMHTTP_H_
 #define IMHTTP_H_
+#include <assert.h>
 
 typedef void *ImHTTP_Socket;
 
 typedef ssize_t (*ImHTTP_Write) (ImHTTP_Socket socket, const void *buf, size_t count);
 
-typedef ssize_t (*ImHTTP_Read) (ImHTTP_Socket socket, const void *buf, size_t count);
+typedef ssize_t (*ImHTTP_Read) (ImHTTP_Socket socket, void *buf, size_t count);
 
 typedef enum {
     IMHTTP_GET,
     IMHTTP_POST,
 } ImHTTP_Method;
 
+#define IMHTTP_RESPONSE_META_CAPACITY 1024
+#define IMHTTP_RESPONSE_BODY_CHUNK_CAPACITY 1024
+
 typedef struct {
     ImHTTP_Socket socket;
     ImHTTP_Write write;
     ImHTTP_Read read;
+
+    char response_meta[IMHTTP_RESPONSE_META_CAPACITY];
+    size_t response_meta_size;
+
+    char response_body_chunk[IMHTTP_RESPONSE_BODY_CHUNK_CAPACITY];
+    size_t response_body_chunk_size;
 } ImHTTP;
 
 void imhttp_req_begin(ImHTTP *imhttp, ImHTTP_Method method, const char *resource);
-void imhttp_req_header(ImHTTP *imhttp, const char *header, const char *header_value);
+void imhttp_req_header(ImHTTP *imhttp, const char *header_name, const char *header_value);
 
 void imhttp_req_header_end(ImHTTP *imhttp);
 
@@ -34,17 +44,68 @@ void imhttp_req_end(ImHTTP *imhttp);
 
 #ifdef IMHTTP_IMPLEMENTATION
 
+static const char *imhttp_method_as_cstr(ImHTTP_Method method)
+{
+    switch (method){
+        case IMHTTP_GET:
+            return "GET";
+            break;
+        case IMHTTP_POST:
+            return "POST";
+            break;
+        default:
+            assert(0 && "imhttp_method_as_cstr: unreachable\n");
+    }
+}
+
+static void imhttp_write_cstr(ImHTTP *imhttp, const char *cstr)
+{
+    size_t cstr_size = strlen(cstr);
+
+    // TODO: imhttp_write_cstr does not handle ImHTTP_Write errors
+    imhttp->write(imhttp->socket, cstr, cstr_size);
+}
+
+
 void imhttp_req_begin(ImHTTP *imhttp, ImHTTP_Method method, const char *resource)
 {
 
+    imhttp_write_cstr(imhttp, imhttp_method_as_cstr(method));
+    imhttp_write_cstr(imhttp, " ");
+    imhttp_write_cstr(imhttp, resource);
+    imhttp_write_cstr(imhttp, " HTTP/1.0\r\n");
 }
-void imhttp_req_header(ImHTTP *imhttp, const char *header, const char *header_value);
 
-void imhttp_req_header_end(ImHTTP *imhttp);
+void imhttp_req_header(ImHTTP *imhttp, const char *header_name, const char *header_value)
+{
 
-void imhttp_req_body_chunk(ImHTTP *imhttp, const char *chunk_cstr);
-void imhttp_req_body_chunk_size(ImHTTP *imhttp, const char *chunk, size_t chunk_size);
-void imhttp_req_end(ImHTTP *imhttp);
+    imhttp_write_cstr(imhttp, header_name);
+    imhttp_write_cstr(imhttp, ": ");
+    imhttp_write_cstr(imhttp, header_value);
+    imhttp_write_cstr(imhttp, "\r\n");
+}
 
+void imhttp_req_header_end(ImHTTP *imhttp)
+{
+    imhttp_write_cstr(imhttp, "\r\n");
+}
+
+void imhttp_req_body_chunk(ImHTTP *imhttp, const char *chunk_cstr)
+{
+
+    imhttp_write_cstr(imhttp, chunk_cstr);
+}
+
+void imhttp_req_body_chunk_size(ImHTTP *imhttp, const char *chunk, size_t chunk_size)
+{
+
+    imhttp->write(imhttp->socket, chunk, chunk_size);
+}
+
+void imhttp_req_end(ImHTTP *imhttp)
+{
+    (void) imhttp;
+
+}
 
 #endif //IMHTTP_IMPLEMENTATION
--- main.c
+++ main.c
@@ -1,32 +1,82 @@
+#define _POSIX_C_SOURCE 200112L
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define IMHTTP_IMPLEMENTATION
+#include "imhttp.h"
+
+#define HOST "localhost"
+#define PORT "6969"
+
+
+ssize_t imhttp_write(ImHTTP_Socket socket, const void *buf, size_t count)
+{
+    return write((int) (int64_t) socket, buf, count);
+}
+
+
+ssize_t imhttp_read(ImHTTP_Socket socket, void *buf, size_t count)
+{
+    return read((int) (int64_t) socket, buf, count);
+}
+
 
 int main()
 {
-    HTTP http = {};
 
-    const char *url = "http://127.0.0.1:4433";
-    http_begin(&http, HOST, url);
-    {    
-        http_req_begin(&http, HTTP_GET);
-        {
-            http_req_header(&http, "Auth", "Authorize %s", token);
-            http_req_start_body(&http);
-            http_req_body_chunk(&http, "<h1>Hi from C!</h1>");
-        }
-        http_req_end(&http);
+    struct addrinfo hints = {0};
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
 
-        http_res_begin(&http);
-        {
-            if (http.status > 300){
-                url = http.new_location;
-                http_res_end
-            }
-            while (...){
-                http_res_read_chunk(&http);
-            }
-        }
-        http_res_end(&http);
+    struct addrinfo *addrs;
+    if (getaddrinfo(HOST, PORT, &hints, &addrs) < 0) {
+        fprintf(stderr, "Could not get address of `"HOST"` : %s\n", strerror(errno));
+        exit(1);
     }
-    http_end(&http);
+    int sd = 0;
+    for (struct addrinfo *addr = addrs; addr != NULL; addr = addr->ai_next){
+        sd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
 
+        if (sd == -1) break;
+        if (connect(sd, addr->ai_addr, addr->ai_addrlen) == 0) break;
+        close(sd);
+        sd = -1;
+    }
+
+    freeaddrinfo(addrs);
+
+    if(sd == -1) {
+        fprintf(stderr, "Could not get address of `"HOST"` : %s\n", strerror(errno));
+        exit(1);
+    }
+
+    ImHTTP imhttp = {
+        .socket = (void *) (int64_t) sd,
+        .write = imhttp_write,
+        .read = imhttp_read,
+    };
+
+    imhttp_req_begin(&imhttp, IMHTTP_GET, "/index.html");
+    {
+        imhttp_req_header(&imhttp, "Host", HOST);
+        imhttp_req_header(&imhttp, "Foo", "Bar");
+        imhttp_req_header(&imhttp, "Hello", "Worl");
+        imhttp_req_header_end(&imhttp);
+        imhttp_req_body_chunk(&imhttp, "Hello Body World!\n");
+
+    }
+    imhttp_req_end(&imhttp);
+    close(sd);
     return 0;
 }
